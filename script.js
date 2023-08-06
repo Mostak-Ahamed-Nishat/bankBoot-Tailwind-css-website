70189f0c86bdcaddf620e18907e1e339eff39037
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2023-08-06 14:55:03 +0600

    pricing section added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = () => {
     let userPics = document.getElementsByClassName('user-pic')
     let userTexts = document.getElementsByClassName('user-text')
     let mLink = document.querySelectorAll('#mLink')
+    let toggleButton = document.getElementById('toggleButton')
 
     hamburgerButton.addEventListener('click', toggleHamburgerButton)
 
@@ -18,13 +19,32 @@ window.onload = () => {
         lnk.addEventListener('click', toggleHamburgerButton)
     });
 
+   //Pricing toggle 
+
+   const card_1_font = document.querySelector('#card-1-front')
+   const card_1_back = document.querySelector('#card-1-back')
+
+   const card_2_font = document.querySelector('#card-2-front')
+   const card_2_back = document.querySelector('#card-2-back')
+
+   const card_3_font = document.querySelector('#card-3-front')
+   const card_3_back = document.querySelector('#card-3-back')
+
+   toggleButton.addEventListener('change', () => {
+       card_1_font.classList.toggle('-rotate-y-180')
+       card_1_back.classList.toggle('rotate-y-180')
+
+       card_2_font.classList.toggle('-rotate-y-180')
+       card_2_back.classList.toggle('rotate-y-180')
+
+       card_3_font.classList.toggle('-rotate-y-180')
+       card_3_back.classList.toggle('rotate-y-180')
+   })
     // User review
     return showReview = function (e) {
-
         for (userPic of userPics) {
             userPic.classList.remove('border-active');
         }
-
         for (userText of userTexts) {
             userText.classList.remove('active-review');
         }
@@ -33,5 +53,7 @@ window.onload = () => {
         userPics[index].classList.add('border-active');
         userTexts[index].classList.add('active-review');
     }
+
+ 
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+const rotateY = plugin(function ({
+  addUtilities
+}) {
+  addUtilities({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)",
+    }
+  }),
+  addUtilities({
+    '.-rotate-y-180': {
+      transform: "rotateY(180deg)",
+    }
+  })
+})
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
@@ -21,5 +38,5 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [rotateY],
 }
